13432247355e899ec3d184ecf6d6a3f592602edc
Circle <circle811@gmail.com>
2018-05-31 12:05:49 +0800

    modify assignment1/cs231n/classifiers/linear_classifier.py

--- assignment1/cs231n/classifiers/linear_classifier.py
+++ assignment1/cs231n/classifiers/linear_classifier.py
@@ -51,7 +51,7 @@ class LinearClassifier(object):
       # Hint: Use np.random.choice to generate indices. Sampling with         #
       # replacement is faster than sampling without replacement.              #
       #########################################################################
-      index = np.random.choice(num_train, batch_size, replace=False)
+      index = np.random.choice(num_train, min(num_train, batch_size), replace=False)
       X_batch = X[index]
       y_batch = y[index]
       #########################################################################
